21f502a734ca78fd7edd75093256814de3a2e79c
ashwins97 <47807915+ashwins97@users.noreply.github.com>
2019-02-20 14:27:20 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int inp[30],i,n;
+    int rise=0;
+    int fall=0;
+    int constant=0;
+    printf("enter the length of sequence \n");
+    scanf("%d",&n);
+    printf("enter the binary sequence \n");
+    for( i=0;i<n;i++)
+    {
+
+        scanf("%d",&inp[i]);
+    }
+
+    for(i=1;i<n;i++)
+    {
+
+
+    if(inp[i]<inp[i+1])
+    {
+
+        rise++;
+
+    }
+
+    else if(inp[i]>inp[i+1])
+    {
+
+        fall++;
+
+    }
+    else if(inp[i]==inp[i+1])
+    {
+
+        constant++;
+
+    }
+
+    }
+    printf("the number of rising edges are %d \n",rise);
+    printf("the number of falling  edges are %d \n",fall);
+    printf("the number of constants are %d \n",constant);
+    return 0;
+}
